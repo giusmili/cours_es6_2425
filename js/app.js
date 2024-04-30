3762699ce364c24ba2e6b70ef6f87e3cf44354b6
giusmili <giusmili67@gmail.com>
2024-04-30 15:31:54 +0200

    js tab

--- js/app.js
+++ js/app.js
@@ -57,7 +57,43 @@
         console.log(cle.nom+' '+cle.prenom)
     })
 
-    //opérateurs
+    //opérateurs = + - * / % > < ? ?? : 
+     let age = 24
+     let annee = 2024
+     console.log(age% 2)
+
+     console.log(`Vous êtes né en ${annee - age}`)
+     age% 2 == 0 ? console.log("Nombre paire") : console.log("nombre impaire")
+
+     let prix = 55 //ht 55 * 5.5 / 100
+     let taxe = 5.5
+
+     console.log(`Le prix TTC ${prix*taxe / 100 + prix}`)
+     // ttc
+
+     
+const product=[
+    {name: "car", price: 6000},
+    {name: "Bike", price: 150},
+    {name: "phone", price: 700},
+    {name: "Watch", price: 200},
+    {name: "pen", price: 8},
+    {name: "bag", price: 50},
+];
+
+console.log(product)
+
+console.log(Object.values(product[0]))
+
+console.log(product[1].price)
+/* product.map((cle) =>{
+    console.log(cle.name+' '+cle.price)
+}) */
+for(let data of product){
+    console.log(data.name)
+}
+const filterRedItems = product.filter((row) => row.price  < 100)
+console.table(filterRedItems)
 
 })()
 
