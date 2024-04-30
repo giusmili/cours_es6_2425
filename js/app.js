d75b02b15436b330f40e43c23db62a705d30a500
giusmili <giusmili67@gmail.com>
2024-04-30 12:46:32 +0200

    js

--- js/app.js
+++ js/app.js
@@ -9,9 +9,53 @@
     const bool = true
     const x = null
     console.log(typeof nom)
-    const langs = []
-    console.log(typeof langs)
-    
+    const langs = ["html","css","js", true]
+    console.table(langs)
+  
+   
+    console.log(langs.length)
+    /* for(let i = 0; i < langs.length; i++){
+        console.log(langs[i])
+    } */
+    let i = 0;
+    while(i < langs.length){
+        console.log(`Index ${i} : ${langs[i]}`)
+        i++
+    }
+    const User = {
+        nom: "Doe",
+        prenom: "John",
+        age: 30,
+        ville: "NY",
+        pays: "USA"
+    }
+    console.log(Object.values(User));
+    for(let index in User){
+        console.log(`${index} : ${User[index]}`)
+    }
+   
+    langs.forEach((element) => {
+        console.log(element)
+    });
+    const persons =[
+        {
+            nom : "Berner Lee",
+            prenom : "Tim"
+        },
+        {
+            nom : "Each",
+            prenom: "Branden"
+        },
+        {
+            nom : "Doe",
+            prenom: "John"
+        }
+    ]
+    console.log(Object.values(persons));
+    console.table(persons)
+    persons.forEach((cle) =>{
+        console.log(cle.nom+' '+cle.prenom)
+    })
 })()
 
 
